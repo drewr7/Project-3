a3342bc4cd2c3cfeedf157f2fa751914156e9bd1
DrewReynolds <drew@sportscrm.com>
2019-12-05 18:12:19 -0600

    Remove un needed dev dependencies

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -1,25 +1,7 @@
 import React, { Component } from "react";
-import Faker from "faker";
-import moment from "moment";
 import MainContent from "../components/MainContent"
 
 class MainPage extends Component {
-  state = {
-    users: []
-  };
-  componentDidMount() {
-    let arr = new Array(10).fill(undefined).map((val, idx) => {
-      let user = {
-        firstName: Faker.name.firstName(),
-        lastName: Faker.name.lastName(),
-        profilePicture: Faker.image.avatar(),
-        lastSignedIn: moment(Faker.date.recent(45)).fromNow(),
-        latestPost: Faker.lorem.paragraphs(2, " ")
-      };
-      return user;
-    });
-    this.setState({ users: arr });
-  }
   render() {
     return (
       <React.Fragment>
